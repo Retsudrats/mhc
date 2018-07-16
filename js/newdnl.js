0531c36bfb22ae59c6550f934cb3dbe61d05133f
Retsudrats <lockedinacrystal@live.com>
2018-07-16 05:20:09 -0400

    adding editted files
    Uploading first set.

---
+++ js/newdnl.js
@@ -0,0 +1,78 @@
+var i = 0;
+var t = 0;
+var nlclass = "class";
+var nlclassPatch = "class";
+
+nltree[i] = "Druid"; i++;
+nltree[i] = "Priest"; i++;
+nltree[i] = "Occultist"; i++;
+
+i = 0;
+
+//balance talents
+nltalent[i] = ["Starlight Wrath"]; i++;
+nltalent[i] = ["Genesis"]; i++;
+nltalent[i] = ["Moonglow"]; i++;
+nltalent[i] = ["Nature's Mastery"]; i++;
+nltalent[i] = ["Improved Moonfire"]; i++;
+nltalent[i] = ["Brambles"]; i++;
+nltalent[i] = ["Nature's Grace"]; i++;
+nltalent[i] = ["Nature's Splendor"]; i++;
+nltalent[i] = ["Nature's Reach"]; i++;
+nltalent[i] = ["Vengeance"]; i++;
+nltalent[i] = ["Celestial Focus"]; i++;
+nltalent[i] = ["Lunar Guidance"]; i++;
+nltalent[i] = ["Insect Swarm"]; i++;
+nltalent[i] = ["Improved Insect Swarm"]; i++;
+nltalent[i] = ["Dreamstate"]; i++;
+nltalent[i] = ["Moonfury"]; i++;
+nltalent[i] = ["Balance of Power"]; i++;
+treeStartStop[t] = i -1;
+t++;
+
+//feral talents
+nltalent[i] = ["Ferocity"]; i++;
+nltalent[i] = ["Feral Aggression"]; i++;
+nltalent[i] = ["Feral Instinct"]; i++;
+nltalent[i] = ["Savage Fury"]; i++;
+nltalent[i] = ["Thick Hide"]; i++;
+nltalent[i] = ["Feral Swiftness"]; i++;
+nltalent[i] = ["Survival Instincts"]; i++;
+nltalent[i] = ["Sharpened Claws"]; i++;
+nltalent[i] = ["Shredding Attacks"]; i++;
+nltalent[i] = ["Predatory Strikes"]; i++;
+nltalent[i] = ["Primal Fury"]; i++;
+nltalent[i] = ["Primal Precision"]; i++;
+nltalent[i] = ["Brutal Impact"]; i++;
+nltalent[i] = ["Feral Charge"]; i++;
+nltalent[i] = ["Nurturing Instinct"]; i++;
+nltalent[i] = ["Natural Reaction"]; i++
+nltalent[i] = ["Heart of the Wild"]; i++;
+
+treeStartStop[t] = i -1;
+t++;
+
+//restoration talents
+
+nltalent[i] = ["Improved Mark of the Wild"]; i++;
+nltalent[i] = ["Nature's Focus"]; i++;
+nltalent[i] = ["Furor"]; i++;
+nltalent[i] = ["Naturalist"]; i++;
+nltalent[i] = ["Subtlety"]; i++;
+nltalent[i] = ["Natural Shapeshifter"]; i++;
+nltalent[i] = ["Intensity"]; i++;
+nltalent[i] = ["Omen of Clarity"]; i++;
+nltalent[i] = ["Master Shapeshifter"]; i++;
+nltalent[i] = ["Tranquil Spirit"]; i++;
+nltalent[i] = ["Improved Rejuvenation"]; i++;
+nltalent[i] = ["Nature's Swiftness"]; i++;
+nltalent[i] = ["Gift of Nature"]; i++;
+nltalent[i] = ["Improved Tranquility"]; i++;
+nltalent[i] = ["Empowered Touch"]; i++;
+nltalent[i] = ["Improved Regrowth"]; i++;
+nltalent[i] = ["Living Spirit"]; i++;
+
+treeStartStop[t] = i -1;
+t++;
+
+jsLoaded=true;//needed for ajax script loading
